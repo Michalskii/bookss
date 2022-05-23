1f5a9909c782ea4045cbf72af8fb329cf6f56690
Michalskii <94405347+Michalskii@users.noreply.github.com>
2022-05-23 18:53:34 +0200

    try/catch

--- src/components/BooksDetails.vue
+++ src/components/BooksDetails.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <v-card-subtitle class="display-1 font-italic"
-      >{{ book.title }} -- Released: {{ book.released }}</v-card-subtitle
+    <v-card-subtitle class="font-italic"
+      >Title - {{ book.title }}</v-card-subtitle
     >
     <v-card-text
-      ><p class="">{{ book.author }}</p>
-      <p>Fantasy {{ book.genreId }}</p></v-card-text
-    >
+      ><p>Released: {{ book.released }}</p>
+      <p>Genre- {{ genreName }}</p>
+      <p>
+        Author -
+        <span>{{ authorName }} </span
+        ><span class="caption"> / Born: {{ authorYear }}</span>
+      </p>
+    </v-card-text>
   </div>
 </template>
 
@@ -16,19 +21,45 @@ import { mapState } from "vuex";
 export default {
   props: ["book"],
   computed: {
-    // books() {
-    //   return this.$store.state.booksStore.books;
-    // },
-    // authors() {
-    //   return this.$store.state.authorsStore.authors;
-    // },
-    // genres() {
-    //   return this.$store.state.genresStore.genres;
-    // },
-    // ...mapState("booksStore", ["books"]),
-    // ...mapState("genresStore", ["genres"]),
-    // ...mapState("authorsStore", ["authors"]),
+    ...mapState("booksStore", ["books"]),
+    ...mapState("genresStore", ["genres"]),
+    ...mapState("authorsStore", ["authors"]),
+
+    authorName() {
+      try {
+        const name = this.authors.find(
+          ({ id }) => id === this.book.author
+        ).fullName;
+
+        return name;
+      } catch {
+        return "Who knows?";
+      }
+    },
+    authorYear() {
+      try {
+        const name = this.authors.find(
+          ({ id }) => id === this.book.author
+        ).yearBorn;
+
+        return name;
+      } catch {
+        return "Long time ago";
+      }
+    },
+    genreName() {
+      try {
+        const name = this.genres.find(
+          ({ id }) => id === this.book.genreId
+        ).name;
+
+        return name;
+      } catch {
+        return "Unknown";
+      }
+    },
   },
+  methods: {},
 };
 </script>
 <style>
